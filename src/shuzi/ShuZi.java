9e5fffada024aea5e116bf8becb8ebd3cb4d7135
YuZhiQin5560 <1308785589@qq.com>
2018-06-19 12:13:33 +0800

    ceshi

---
+++ src/shuzi/ShuZi.java
@@ -0,0 +1,36 @@
+package shuzi;
+
+public class ShuZi {
+	
+	public static void main(String[] args) {
+		
+		/*int b = 100;
+		int g = b++;
+		System.err.println("g:" + g);
+		
+		int c = 100;
+		int h = ++c;
+		System.err.println("h:" + h);
+		
+		int f = 200;
+		int i = f--;
+		System.err.println("i:" + i);
+		
+		int e = 200;
+		int j = --e;
+		System.err.println("j:" + j);
+		
+		int t = 3;
+		int a = t << 3;
+		System.err.println("a:"+a);*/
+		
+		int a , b;
+		a = 10;
+		
+		b = (a == 1) ? 20 : 30;
+		System.err.println("b:" + b);
+		
+		b = (a == 10) ? 20 : 30;
+		System.err.println("b:" + b);
+	}
+}
